c4dec1c7cc505fcbf8b21d03036b3c6e139de1f1
Danilo Tomasoni <tomasoni@cosbi.eu>
2023-10-30 12:09:20 +0100

    test also env params

--- tests/integration/odeset/main.m
+++ tests/integration/odeset/main.m
@@ -3,8 +3,9 @@ options = odeset('RelTol',1e-5,'AbsTol',1e-6);
 
 tspan = 0:0.1:2;
 y0 = [3];
+base = 10;
 
-[t,y] = ode15s(@(t,y) -10*y, tspan, y0, options);
+[t,y] = ode15s(@(t,y) -base*y, tspan, y0, options);
 
 disp(t)
 disp(y)
